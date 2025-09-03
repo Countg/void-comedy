97387a2455fb6a65fb783f7d6041bef08385f6c1
Gavin Stephens <gstephens090@gmail.com>
2025-09-03 12:39:10 -0400

    one more youtube link

--- src/app/lib/projectInfo.js
+++ src/app/lib/projectInfo.js
@@ -1,31 +1,24 @@
-import {FaSpotify, FaApple, FaYoutube, FaPodcast} from 'react-icons/fa';
+import { FaSpotify, FaApple, FaYoutube, FaPodcast } from "react-icons/fa";
 
-
-
-
-
-
-
-    export const platforms = [
-      {
-        name: "Spotify",
-        url:  "https://open.spotify.com/show/4VXk56ZMQABGFxlu6aBuUv",
-        icon: FaSpotify,
-      },
-      {
-        name: "Apple Podcasts",
-        url:  "https://podcasts.apple.com/us/podcast/uncolonized/id698940847",
-        icon: FaApple,
-      },
-      {
-        name: "YouTube",
-        url:  "https://www.youtube.com/@GavinStephens",
-        icon: FaYoutube,
-      },
-      {
-        name: "Acast",
-        url:  "https://shows.acast.com/uncolonized",
-        icon: FaPodcast, // Using Spotify icon for Acast as placeholder 
-      }
-    ]
- 
+export const platforms = [
+  {
+    name: "Spotify",
+    url: "https://open.spotify.com/show/4VXk56ZMQABGFxlu6aBuUv",
+    icon: FaSpotify,
+  },
+  {
+    name: "Apple Podcasts",
+    url: "https://podcasts.apple.com/us/podcast/uncolonized/id698940847",
+    icon: FaApple,
+  },
+  {
+    name: "YouTube",
+    url: "https://www.youtube.com/@ParkBenchOntology",
+    icon: FaYoutube,
+  },
+  {
+    name: "Acast",
+    url: "https://shows.acast.com/uncolonized",
+    icon: FaPodcast, // Using Spotify icon for Acast as placeholder
+  },
+];
